b0b8fff3edef8023041708cb941180d9368ee284
parm530 <parmsahadeo530@gmail.com>
2016-07-12 12:43:40 -0400

    Implemented #all? using yield

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,17 @@
 require 'pry'
 
 def my_all?(collection)
+  block_return_values = []
+  i = 0
+  while(i < collection.size)
+    block_return_values << yield(collection[i])
+    i += 1
+  end
+
+  if(block_return_values.include?(false) == true)
+    false
+  else
+    true
+  end
 
 end
